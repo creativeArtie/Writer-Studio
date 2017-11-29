5c18ea1747e342693082120cdc3454211aa87c76
creativeArtie <waikinarthur@gmail.com>
2017-11-28 20:48:06 -0800

    Add the ability to print raw text file without coloring.

--- src/com/creativeartie/jwriter/lang/markup/pdf/PDFOutputStream.java
+++ src/com/creativeartie/jwriter/lang/markup/pdf/PDFOutputStream.java
@@ -15,18 +15,20 @@ import com.google.common.base.*;
 public final class PDFOutputStream implements AutoCloseable{
     private static final int TOP = 700;
     private static final int MARGIN = 40;
-    private static final int LINE = -15;
+    private static final int FONT_SIZE = 12;
 
     private PDDocument outDoc;
     private PDPage curPage;
     private PDPageContentStream outStream;
     private String fileName;
-    private float startX;
-    private float startY;
-    private float pageWidth;
+    private final float startX;
+    private final float startY;
+    private final float pageWidth;
+    private float pageHeight;
     private float lineWidth;
+    private float lineHeight;
 
-    public PDFOutputStream(File file) throws IOException{
+    public PDFOutputStream(File file, PDFont font) throws IOException{
         fileName = file.getPath();
         outDoc = new PDDocument();
         PDPage page = new PDPage();
@@ -37,10 +39,13 @@ public final class PDFOutputStream implements AutoCloseable{
         startX = mediabox.getLowerLeftX() + MARGIN;
         startY = mediabox.getUpperRightY() - MARGIN;
         lineWidth = 0f;
+        lineHeight = ((font.getFontDescriptor().getCapHeight()) / 1000 *
+            FONT_SIZE) * 2;
         newPage(page);
     }
 
     private void newPage(PDPage page) throws IOException{
+        pageHeight = startY;
         curPage = page;
         outDoc.addPage(curPage);
         outStream = new PDPageContentStream(outDoc, curPage);
@@ -48,9 +53,14 @@ public final class PDFOutputStream implements AutoCloseable{
         outStream.newLineAtOffset(startX, startY);
     }
 
-    private void newLine() throws IOException{
-        System.out.println();
-        outStream.newLineAtOffset(0, LINE);
+    private void newLine(PDFont font) throws IOException{
+        if (pageHeight - lineHeight < MARGIN){
+            endPage();
+            newPage(new PDPage());
+            outStream.setFont(font, FONT_SIZE);
+        }
+        pageHeight -= lineHeight;
+        outStream.newLineAtOffset(0, -lineHeight);
         lineWidth = 0f;
     }
 
@@ -67,50 +77,53 @@ public final class PDFOutputStream implements AutoCloseable{
 
 
     public void print(String text, PDFont font) throws IOException{
-        print(text, font, 12);
+        showLines(text, font);
     }
 
-    public void print(String text, PDFont font, int size) throws IOException{
-        printString(text, font, size);
+    public void println(PDFont font) throws IOException{
+        newLine(font);
     }
 
     public void println(String text, PDFont font) throws IOException{
-        println(text, font, 12);
+        showLines(text, font);
+        newLine(font);
     }
 
-    public void println(String text, PDFont font, int size) throws IOException{
-        printString(text, font, size);
-        newLine();
-    }
-
-    private void printString(String text, PDFont font, int size)
-            throws IOException{
-        outStream.setFont(font, size);
+    private void showLines(String text, PDFont font) throws IOException{
+        outStream.setFont(font, FONT_SIZE);
         List<String> lines = Splitter.on('\n').splitToList(text);
         if (lines.size() == 1){
-            showText(lines.get(0), font, size);
+            showText(lines.get(0), font);
         } else {
             for(String line: lines){
-                showText(line, font, size);
-                newLine();
+                showText(line, font);
+                newLine(font);
             }
         }
     }
 
-    private void showText(String line, PDFont font, int size) throws IOException{
+    private void showLine(String line, PDFont font) throws IOException{
+        int last = 0;
+        int next = line.indexOf(' ') + 1;
+        while (next != -1){
+            print(line.substring(last, next), font);
+            last = next + 1;
+            next = line.indexOf(' ', last);
+        }
+        if (next < -1){
+            print(line.substring(last), font);
+        }
+    }
 
-        for(String text: Splitter.on(' ').omitEmptyStrings().split(line)){
-            String out = text + ' ';
-            float tmp = lineWidth + (size * font.getStringWidth(out) / 1000);
-            if(tmp < pageWidth){
-                lineWidth = tmp;
-                System.out.println(lineWidth + "\t" + pageWidth + ":" + text);
-                outStream.showText(out + " ");
-            } else {
-                newLine();
-                System.out.println(lineWidth + "\t" + pageWidth + ":" + text);
-                outStream.showText(out + " ");
-            }
+    private void showText(String text, PDFont font) throws IOException{
+        float tmp = lineWidth + (FONT_SIZE * font.getStringWidth(text) / 1000);
+        if (tmp < pageWidth){
+            lineWidth = tmp;
+            outStream.showText(text);
+        } else {
+            newLine(font);
+            newLine(font);
+            outStream.showText(text);
         }
     }
 }
