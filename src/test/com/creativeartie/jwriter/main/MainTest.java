cc5ccce98b313e2334f0f4635bad3c39438ee42c
Wai-Kin Chau <waikinarthur@gmail.com>
2018-02-07 14:50:05 -0800

    Add iText, remove pdfbox and move Apache ODF Toolkit.

--- src/test/com/creativeartie/jwriter/main/MainTest.java
+++ src/test/com/creativeartie/jwriter/main/MainTest.java
@@ -2,14 +2,38 @@ package com.creativeartie.jwriter.main;
 
 import java.io.*;
 
-import com.creativeartie.jwriter.lang.markup.*;
-// import com.creativeartie.jwriter.lang.markup.pdf.*;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainTest {
-    public static void main(String[] args) throws Exception {
-        /*File file = new File("data/help-text.txt");
-        WritingText doc = new WritingText(file);
-        BuildPDF pdf = new BuildPDF(doc);
-        pdf.export(new File("../test.pdf"));*/
+
+    public static final String DEST = "results/chapter01/hello_world.pdf";
+
+    public static void main(String args[]) throws IOException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new MainTest().createPdf(DEST);
+    }
+
+    public void createPdf(String dest) throws IOException {
+        //Initialize PDF writer
+        PdfWriter writer = new PdfWriter(dest);
+
+        //Initialize PDF document
+        PdfDocument pdf = new PdfDocument(writer);
+
+        // Initialize document
+        Document document = new Document(pdf);
+
+        //Add paragraph to the document
+        document.add(new Paragraph("Hello World!"));
+
+        //Close document
+        document.close();
     }
 }
