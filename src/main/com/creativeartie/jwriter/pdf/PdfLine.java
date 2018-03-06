6ef152b76b199505e4abe4a28f83598d2d2d3a0c
Wai-Kin Chau <waikinarthur@gmail.com>
2018-03-06 10:34:10 -0800

    Implmented PdfLine and PdfDiv classes.

--- src/main/com/creativeartie/jwriter/pdf/PdfLine.java
+++ src/main/com/creativeartie/jwriter/pdf/PdfLine.java
@@ -1,17 +1,63 @@
 package com.creativeartie.jwriter.pdf;
 
+import java.io.*;
 import java.util.*;
 
 import org.apache.pdfbox.pdmodel.font.*;
 
 class PdfLine{
     private ArrayList<PdfText> inputText;
-    private float pageWidth;
-    private int firstLineIndent;
-    private int baseIndent;
+    private float maxWidth;
+    private float curWidth;
+    private float textLeading;
+    private float textHeight;
 
-    public PdfLine(float width){
-        inputText = new ArrayList<>();
-        pageWidth = width;
+    public PdfLine(float width, float leading){
+        this(width, leading, new ArrayList<>());
+    }
+
+    public PdfLine(float width, float leading, ArrayList<PdfText> text){
+        inputText = text;
+        maxWidth = width;
+        textLeading = leading;
+        textHeight = 0;
+        curWidth = 0;
+    }
+
+    public ArrayList<PdfText> appendText(String string, PDFont font, int size)
+            throws IOException{
+        ArrayList<PdfText> ans = PdfText.createWords(string, font, size);
+        ArrayList<PdfText> overflow = null;
+        for (PdfText text: ans){
+            if (overflow == null){
+                if (curWidth + text.getWidth() > maxWidth){
+                    overflow = new ArrayList<>();
+                    int last = inputText.size() - 1;
+                    if (! inputText.get(last).isSpaceText()){
+                        overflow.add(inputText.remove(last));
+                    }
+                    overflow.add(text);
+                    curWidth += text.getWidth();
+                } else {
+                    if (text.getHeight() > textHeight){
+                        textHeight = text.getHeight();
+                    }
+                    if (! inputText.isEmpty() || ! text.isSpaceText()){
+                        inputText.add(text);
+                    }
+                }
+            } else {
+                overflow.add(text);
+            }
+        }
+        return overflow == null? new ArrayList<>(): overflow;
+    }
+
+    public float getHeight(){
+        return textHeight * textLeading;
+    }
+
+    public float getWidth(){
+        return curWidth;
     }
 }
--- src/main/com/creativeartie/jwriter/pdf/PdfText.java
+++ src/main/com/creativeartie/jwriter/pdf/PdfText.java
@@ -1,31 +1,71 @@
 package com.creativeartie.jwriter.pdf;
 
+import java.util.*;
 import java.io.*;
 import org.apache.pdfbox.pdmodel.font.*;
+import com.google.common.base.*;
 
 class PdfText{
+    private static final String SPACE = " ";
+
+    public static ArrayList<PdfText> createWords(String text, PDFont font,
+            int size) throws IOException{
+
+        PdfText space = new PdfText(SPACE, font, size, true);
+        CharMatcher whitespace = CharMatcher.whitespace();
+
+        ArrayList<PdfText> holder = new ArrayList<>();
+        for (String word: Splitter.on(whitespace).omitEmptyStrings()
+                .split(text)){
+            holder.add(new PdfText(word, font, size, false));
+        }
+        int i = 0;
+        ArrayList<PdfText> ans = new ArrayList<>();
+        if (whitespace.indexIn(text) == 0){
+            ans.add(space);
+        }
+        for (PdfText item: holder){
+            ans.add(item);
+            ans.add(space);
+        }
+        if (whitespace.lastIndexIn(text) == text.length() - 1){
+            ans.add(space);
+        }
+        return ans;
+    }
 
     private PDFont textFont;
     private int fontSize;
     private String outputText;
+    private float textWidth;
+    private float textHeight;
+    private boolean spaceText;
 
-    private PdfText(String word, PDFont font, int size){
+    private PdfText(String word, PDFont font, int size, boolean space)
+            throws IOException{
         outputText = word;
         textFont = font;
         fontSize = size;
+        /// From https://stackoverflow.com/questions/13701017/calculation-string-width-in-pdfbox-seems-only-to-count-characters
+        textWidth = textFont.getStringWidth(outputText) / 1000 * fontSize;
+        /// From https://stackoverflow.com/questions/17171815/get-the-font-height-of-a-character-in-pdfbox
+        textHeight = textFont.getFontDescriptor().getCapHeight() / 1000 * fontSize;
+        spaceText = space;
     }
 
-    public float getWidth() throws IOException{
-        /// From https://stackoverflow.com/questions/13701017/calculation-string-width-in-pdfbox-seems-only-to-count-characters
-        return textFont.getStringWidth(outputText) / 1000 * fontSize;
+    public float getWidth() {
+        return textWidth;
     }
 
     public float getHeight(){
-        /// From https://stackoverflow.com/questions/17171815/get-the-font-height-of-a-character-in-pdfbox
-        return textFont.getFontDescriptor().getCapHeight() / 1000 * fontSize;
+        return textHeight;
     }
 
     public String getText(){
         return outputText;
+    }
+
+    public boolean isSpaceText(){
+        return spaceText;
     }
 }
