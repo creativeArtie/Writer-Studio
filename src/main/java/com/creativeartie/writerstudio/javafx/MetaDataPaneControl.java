8518114cb64b22c2ca44fe112b82ebf992485dcd
Wai-Kin Chau <waikinarthur@gmail.com>
2018-07-08 16:07:43 -0700

    Created javafx-properties diagram.

--- src/main/java/com/creativeartie/writerstudio/javafx/MetaDataPaneControl.java
+++ src/main/java/com/creativeartie/writerstudio/javafx/MetaDataPaneControl.java
@@ -15,17 +15,32 @@ class MetaDataPaneControl extends MetaDataPaneView{
     protected void setupChildern(WriterSceneControl control){
         matterWindow.setupProperties(control);
         control.writingDataProperty().addListener((d, o, n) -> loadData(n));
+        for (TextTypeInfo info: TextTypeInfo.values()){
+			getInfoField(info).setOnAction(e -> editInfo(info));
+		}
         for (TextTypeMatter matter: TextTypeMatter.values()){
             getMatterButton(matter).setOnAction(evt -> editMatter(matter));
         }
     }
 
     private void loadData(WritingData data){
-        for (TextTypeInfo meta: TextTypeInfo.values()){
-            getInfoField(meta).setText(data.getInfo(meta));
-        }
         writingData = data;
+        if (data != null){
+			writingData.addDocEdited(s -> updateData());
+			updateData();
+		}
     }
+    
+    private void updateData(){
+		if (writingData == null) return;
+		for (TextTypeInfo info: TextTypeInfo.values()){
+            getInfoField(info).setText(writingData.getInfo(meta));
+        }
+	}
+    
+    private void editInfo(TextTypeInfo info){
+		writingData.setInfo(info, getInfoField(info).getText());
+	}
 
     private void editMatter(TextTypeMatter matter){
         matterWindow.setShowMatter(matter);
