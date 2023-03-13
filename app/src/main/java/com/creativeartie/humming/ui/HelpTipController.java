0d6777e9fe0b275b0cfa9de9abf98901d59827e2
Wai-Kin <waikinarthur@gmail.com>
2023-03-13 11:52:44 -0700

    Add help tip.

--- app/src/main/java/com/creativeartie/humming/ui/HelpTipController.java
+++ app/src/main/java/com/creativeartie/humming/ui/HelpTipController.java
@@ -5,9 +5,19 @@ import javafx.scene.control.*;
 
 public class HelpTipController {
     @FXML
-    private Label headingText;
+    private Label headingText, outlineText;
     @FXML
-    private Label outlineText;
+    private Label stubStatus, outlineStatus, draftStatus, finalStatus, othersStatus, statusDetails;
+    @FXML
+    private Label quoteLine, todoLine, normalLine, tableLine, breakLine;
+    @FXML
+    private Label footnoteLine, endnoteLine, imageLine, numberedLine;
+    @FXML
+    private Label summaryLine, noteLine, fieldLine, bulletLine;
+    @FXML
+    private Label footRef, endRef, noteRef, metaRef, todoRef;
+    @FXML
+    private Label errRef, boldSpan, underSpan, italicSpan, catSpan, idSpan;
 
     @FXML
     protected void initialize() {}
--- app/src/main/java/com/creativeartie/humming/ui/WritingController.java
+++ app/src/main/java/com/creativeartie/humming/ui/WritingController.java
@@ -9,6 +9,7 @@ import org.fxmisc.richtext.model.*;
 
 import com.creativeartie.humming.document.*;
 
+import javafx.beans.property.*;
 import javafx.beans.value.*;
 import javafx.fxml.*;
 import javafx.geometry.*;
@@ -16,14 +17,37 @@ import javafx.scene.control.*;
 import javafx.stage.*;
 
 public class WritingController {
-    private Document rootDoc;
-    Popup popup;
-    Label popupMsg;
+    private class DocumentProperty extends ReadOnlyObjectWrapper<Document> {
+        DocumentProperty() {
+            super(new Document());
+        }
+
+        private void updateText(String text) {
+            getValue().updateText(text);
+            if (text.isEmpty()) return;
+            final StyleSpansBuilder<Collection<String>> styleSpans = new StyleSpansBuilder<>();
+            styleSpans.addAll(
+                    rootDoc.get().convertLeaves((leaf) -> new StyleSpan<>(leaf.getCssStyles(), leaf.getLength()))
+            );
+            writingText.setStyleSpans(0, styleSpans.create());
+            int line = 0;
+            for (StyleLines style : rootDoc.get().convertLines((para) -> para.getLineStyle())) {
+                writingText.setParagraphStyle(line, List.of(style.getCssName()));
+                line++;
+            }
+            fireValueChangedEvent();
+        }
+    }
+
+    @FXML
+    private DocumentProperty rootDoc;
+    private Popup popup;
+    private Label popupMsg;
     @FXML
     public CodeArea writingText;
 
     public WritingController() {
-        rootDoc = new Document();
+
         popup = new Popup();
         popupMsg = new Label();
         popupMsg.setStyle("-fx-background-color: black;" + "-fx-text-fill: white;" + "-fx-padding: 5;");
@@ -32,7 +56,10 @@ public class WritingController {
 
     @FXML
     public void initialize() {
+
+        rootDoc = new DocumentProperty();
         writingText.textProperty().addListener(this::textEdited);
+
         writingText.setMouseOverTextDelay(Duration.ofSeconds(1));
         writingText.addEventHandler(MouseOverTextEvent.MOUSE_OVER_TEXT_BEGIN, this::mouseOverTextEvent);
         writingText.addEventHandler(MouseOverTextEvent.MOUSE_OVER_TEXT_END, e -> popup.hide());
@@ -42,7 +69,7 @@ public class WritingController {
         int chIdx = e.getCharacterIndex();
         Point2D pos = e.getScreenPosition();
         String name = "";
-        for (Span child : rootDoc.locateChildren(chIdx)) {
+        for (Span child : rootDoc.get().locateChildren(chIdx)) {
             String appendText = child.getClass().getSimpleName();
             if (child instanceof Para) {
                 appendText = ((Para) child).getLineStyle().toString();
@@ -62,14 +89,9 @@ public class WritingController {
 
     private void textEdited(ObservableValue<?> observable, String oldValue, String newValue) {
         rootDoc.updateText(newValue);
-        if (newValue.isEmpty()) return;
-        final StyleSpansBuilder<Collection<String>> styleSpans = new StyleSpansBuilder<>();
-        styleSpans.addAll(rootDoc.convertLeaves((leaf) -> new StyleSpan<>(leaf.getCssStyles(), leaf.getLength())));
-        writingText.setStyleSpans(0, styleSpans.create());
-        int line = 0;
-        for (StyleLines style : rootDoc.convertLines((para) -> para.getLineStyle())) {
-            writingText.setParagraphStyle(line, List.of(style.getCssName()));
-            line++;
-        }
+    }
+
+    public ReadOnlyObjectProperty<Document> documentProperty() {
+        return rootDoc.getReadOnlyProperty();
     }
 }
